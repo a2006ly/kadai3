cdbbd44c08df28ff679c2dc6e9cda508e6b348e6
enbunsui <a1906wy@aiit.ac.jp>
2019-05-23 06:21:09 +0000

    Create Test Case

---
+++ 19745106_kadai3Test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#case 1
+case1=`./19745106_kadai3.sh 8 12`
+echo -e "case1----> \n$case1"
+echo -e "\n"
+
+#case 2
+case2=`./19745106_kadai3.sh 0 22`
+echo -e "case2----> \n$case2"
+echo -e "\n"
+
+#case 3
+case3=`./19745106_kadai3.sh 8 0`
+echo -e "case3----> \n$case3"
+echo -e "\n"
+
+#case 4
+case4=`./19745106_kadai3.sh a 42`
+echo -e "case4----> \n$case4"
+echo -e "\n"
+
+#case 5
+case5=`./19745106_kadai3.sh 8 b`
+echo -e "case5----> \n$case5"
+eecho -e "\n"
+
+#case 6
+case6=`./19745106_kadai3.sh c d`
+echo -e "case6----> \n$case6"
+echo -e "\n"
+
+#case 7
+case7=`./19745106_kadai3.sh`
+echo -e "case7----> \n$case7"
+echo -e "\n"
