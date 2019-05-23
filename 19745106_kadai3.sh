782d55eb2fab96c41e16af3d344883b1e2da66a1
enbunsui <a1906wy@aiit.ac.jp>
2019-05-23 01:54:59 +0000

    add input check

--- 19745106_kadai3.sh
+++ 19745106_kadai3.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 
-
+#params count
 if [ $# -ne 2 ];then
     echo "error"
     exit 1
 fi
 
+#parm1 number check
+if echo "$1" | grep -q "^[0-9]\+$";then
+	echo "$1"
+else
+	echo "NG"
+	exit 1
+fi
+
+#parm2 number check
+if echo "$2" | grep -q "^[0-9]\+$";then
+	echo "$2"
+else
+	echo "NG"
+	exit 1
+fi
+
+#if 0 then 0
 if [ $1 -eq 0 -o $2 -eq 0 ];then
 	echo "gcd is 0"
 	exit 
 fi
 
-a=$1
-b=$2
+vara=$1
+varb=$2
 
 gcd(){
-    while [ $a -ne $b  ]
+    while [ $vara -ne $varb  ]
     do
-	if [ $a -gt $b  ];then
-		a=$(( a-b ))
+	if [ $vara -gt $varb  ];then
+		vara=$(( vara-varb ))
 	else
-		b=$(( b-a ))
+		varb=$(( varb-vara ))
 	fi
     done
-	echo "gcd is $a"
+	echo "gcd is $vara"
 }
 
 gcd $1 $2
