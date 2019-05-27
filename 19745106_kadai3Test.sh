c348d4a5f2baab3c48971da613c73abebd2a1990
enbunsui <50896197+enbunsui@users.noreply.github.com>
2019-05-27 13:14:17 +0900

    Update 19745106_kadai3Test.sh

--- 19745106_kadai3Test.sh
+++ 19745106_kadai3Test.sh
@@ -17,7 +17,7 @@ if [ $? -ne 0 ];then
 else
     echo -e "CASE1正常終了"	
 fi
-    echo -e "\n"
+echo -e "\n"
 
 #case 2 正常系 数字入力
 #入力：引数1=0、引数2=22
@@ -28,7 +28,7 @@ if [ $? -ne 0 ];then
 else
     echo -e "CASE2正常終了"	
 fi
-    echo -e "\n"
+echo -e "\n"
 
 #case 3 正常系 数字入力
 #入力：引数1=8、引数2=0
@@ -39,7 +39,7 @@ if [ $? -ne 0 ];then
 else
     echo -e "CASE3正常終了"	
 fi
-    echo -e "\n"
+echo -e "\n"
 
 #case 4 異常系 数字以外
 #入力：引数1=a、引数2=42
@@ -47,11 +47,10 @@ fi
 ./19745106_kadai3.sh a 42
 if [ $? -ne 0 ];then
     echo -e "CASE4処理エラー、入力数字をチェックしてください。"  
-    exit 0
 else
     echo -e "CASE4正常終了"	
 fi
-    echo -e "\n"
+echo -e "\n"
 
 #case 5 異常系 数字以外入力
 #入力：引数1=8、引数2=b
@@ -59,11 +58,10 @@ fi
 ./19745106_kadai3.sh 8 b
 if [ $? -ne 0 ];then
     echo -e "CASE5処理エラー、入力数字をチェックしてください。"  
-    exit 0
 else
     echo -e "CASE5正常終了"	
 fi
-    echo -e "\n"
+echo -e "\n"
 
 #case 6 異常系 数字以外入力
 #入力：引数1=c、引数2=d
@@ -71,11 +69,10 @@ fi
 ./19745106_kadai3.sh c d
 if [ $? -ne 0 ];then
     echo -e "CASE6処理エラー、入力数字をチェックしてください。"  
-    exit 0
 else
     echo -e "CASE6正常終了"	
 fi
-    echo -e "\n"
+echo -e "\n"
 
 #case 7 異常系 空入力
 #入力：引数1=、引数2=
@@ -83,8 +80,8 @@ fi
 ./19745106_kadai3.sh
 if [ $? -ne 0 ];then
     echo -e "CASE7処理エラー、入力数字をチェックしてください。"  
-    exit 0
+
 else
     echo -e "CASE7正常終了"	
 fi
-    echo -e "\n"
+echo -e "\n"
