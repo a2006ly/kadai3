e6b689ac4e57cbeaf004abf1c2099316a809cf0b
ywsrock <ywsrock@gmail.com>
2019-05-27 10:33:44 +0900

    テストシェル作成

--- 19745106_kadai3Test.sh
+++ 19745106_kadai3Test.sh
@@ -1,36 +1,85 @@
 #!/bin/bash
 
-#case 1
+
+############################################
+#
+#テストシェル
+#
+############################################
+
+#case 1 正常系 数字入力
+#入力：引数1=8、引数2=12
+#想定出力:4
 case1=`./19745106_kadai3.sh 8 12`
-echo -e "case1----> \n$case1"
-echo -e "\n"
+if [ $? = 1 ];then
+　　echo -e "CASE1処理エラー、入力数字をチェックしてください。"  
+　　echo -e "\n"
+else
+    echo -e "CALSE1正常終了"	
+fi
 
-#case 2
+#case 2 正常系 数字入力
+#入力：引数1=0、引数2=22
+#想定出力:0
 case2=`./19745106_kadai3.sh 0 22`
-echo -e "case2----> \n$case2"
-echo -e "\n"
+if [ $? = 1 ];then
+　　echo -e "CASE2処理エラー、入力数字をチェックしてください。"  
+else
+    echo -e "CALSE2正常終了"	
+fi
+　　echo -e "\n"
 
-#case 3
+#case 3 正常系 数字入力
+#入力：引数1=8、引数2=0
+#想定出力:0
 case3=`./19745106_kadai3.sh 8 0`
-echo -e "case3----> \n$case3"
-echo -e "\n"
+if [ $? = 1 ];then
+　　echo -e "CASE3処理エラー、入力数字をチェックしてください。"  
+else
+    echo -e "CALSE3正常終了"	
+fi
+　　echo -e "\n"
 
-#case 4
+#case 4 異常系 数字以外
+#入力：引数1=a、引数2=42
+#想定出力:型チェックエラー
 case4=`./19745106_kadai3.sh a 42`
-echo -e "case4----> \n$case4"
-echo -e "\n"
+if [ $? = 1 ];then
+　　echo -e "CASE4処理エラー、入力数字をチェックしてください。"  
+else
+    echo -e "CALSE4正常終了"	
+fi
+　　echo -e "\n"
 
-#case 5
+#case 5 異常系 数字以外入力
+#入力：引数1=8、引数2=b
+#想定出力:型チェックエラー
 case5=`./19745106_kadai3.sh 8 b`
-echo -e "case5----> \n$case5"
-echo -e "\n"
+if [ $? = 1 ];then
+　　echo -e "CASE5処理エラー、入力数字をチェックしてください。"  
+else
+    echo -e "CALSE5正常終了"	
+fi
+　　echo -e "\n"
 
-#case 6
+#case 6 異常系 数字以外入力
+#入力：引数1=c、引数2=d
+#想定出力:型チェックエラー
 case6=`./19745106_kadai3.sh c d`
-echo -e "case6----> \n$case6"
-echo -e "\n"
+if [ $? = 1 ];then
+　　echo -e "CASE6処理エラー、入力数字をチェックしてください。"  
+else
+    echo -e "CALSE6正常終了"	
+fi
+　　echo -e "\n"
 
-#case 7
+#case 7 異常系 空入力
+#入力：引数1=、引数2=
+#想定出力:パラメーター数チェックエラー
 case7=`./19745106_kadai3.sh`
-echo -e "case7----> \n$case7"
-echo -e "\n"
+if [ $? = 1 ];then
+　　echo -e "CASE7処理エラー、入力数字をチェックしてください。"  
+else
+    echo -e "CALSE7正常終了"	
+fi
+　　echo -e "\n"
